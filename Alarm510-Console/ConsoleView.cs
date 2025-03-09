7278dc2232f6786c60213dc031ff1510b99c2ab6
Connor72 <Connor72@CS-E1114W23>
2025-03-09 16:00:59 -0500

    Program Updated

---
+++ Alarm510-Console/ConsoleView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alarm510_Console
+{
+    public class ConsoleView
+    {
+
+        public ConsoleView() 
+        {
+            
+        }
+
+        /// <summary>
+        /// Updates the alarms in the console view
+        /// </summary>
+        /// <param name="alarms">the list of alarms</param>
+        public void UpdateAlarmView(string[] alarms)
+        {
+            for (int i = 0; i < alarms.Length; i++)
+            {
+                Console.WriteLine(alarms[i]);
+            }
+        }
+
+        
+
+        
+
+
+
+
+
+    }
+}
--- Alarm510-Console/Program.cs
+++ Alarm510-Console/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Alarm510_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+            ConsoleView view = new ConsoleView();
+            
+        }
+    }
+}
