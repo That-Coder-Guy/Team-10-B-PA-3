5b399ed3f191a69177657b66043e739669eb2d78
Connor72 <Connor72@CS-E1114W23>
2025-03-09 16:37:09 -0500

    Merging ConsoleBranch into Main

--- Alarm510-Console/ConsoleView.cs
+++ Alarm510-Console/ConsoleView.cs
@@ -8,10 +8,18 @@ namespace Alarm510_Console
 {
     public class ConsoleView
     {
-
+        /// <summary>
+        /// The default constructor 
+        /// </summary>
         public ConsoleView() 
         {
-            
+            Console.WriteLine("Welcome to CIS 501 - Alarms");
+            Console.WriteLine("___________________________");
+            Console.WriteLine("This is the current alarm list:\n");
+            // Call delegate here to update the list of alarms
+
+          
+
         }
 
         /// <summary>
@@ -26,12 +34,87 @@ namespace Alarm510_Console
             }
         }
 
-        
+        /// <summary>
+        /// The instructions on how to perform certain operations in the alarm
+        /// </summary>
+        public void AlarmInstructions()
+        {
+            Console.WriteLine("\nTo add an alarm, type 'add'.");
 
-        
+            Console.WriteLine("To edit an alarm, type 'edit'.");
+
+            Console.WriteLine("To stop an alarm from going off, type 'stop'.");
+
+            Console.WriteLine("To snooze an alarm, type 'snooze'.");
+
+            Console.WriteLine("To get instructions for the console, type 'help'.");
+
+            Console.WriteLine("To fully exit the program, type 'exit'.\n");
+        }
 
 
+        /// <summary>
+        /// Waits for the user to input a command
+        /// </summary>
+        public string AwaitUserInput()
+        {
+            Console.Write("Input here: ");
+            string input = "";
+            do
+            {
+                input = Console.ReadLine() ?? "";
 
+            } while (!ValidInput(input.ToLower()));
+            
+            return input;
+            
+            
+        }
+        /// <summary>
+        /// Checks if the user input is an acceptable input 
+        /// </summary>
+        /// <param name="input">the input the user entered</param>
+        /// <returns></returns>
+        public bool ValidInput(string input)
+        {
+            
+            if (input != null && input.Length > 0)
+            {
+                if (input.Equals("add") || input.Equals("edit") || input.Equals("stop") || input.Equals("snooze") || input.Equals("exit") || input.Equals("help"))
+                {
+                    return true;
+                }
+                
+            }
+            Console.Write("\nInvalid input, try again.\nInput here: ");
+            return false;
+        }
+
+        /// <summary>
+        /// Manages which function delegates the user should call
+        /// </summary>
+        /// <param name="input">User input.</param>
+        public void Manager(string input)
+        {
+            switch (input)
+            {
+                case "add":
+                    // call add instructions
+                    break;
+                case "edit":
+                    // call edit instructions
+                    break;
+                case "stop":
+                    // call stop instructions
+                    break;
+                case "snooze":
+                    // call snooze instructions
+                    break;
+                case "help":
+                    AlarmInstructions();
+                    break;
+            }
+        }
 
 
     }
--- Alarm510-Console/Program.cs
+++ Alarm510-Console/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 namespace Alarm510_Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-
             ConsoleView view = new ConsoleView();
-            
+
+            //  Will be in a for loop
+            view.AlarmInstructions();
+            while (true)
+            {
+                
+                string input = view.AwaitUserInput();
+
+                if (input.Equals("exit"))
+                {
+                    Environment.Exit(0);
+                }
+                view.Manager(input);
+
+            }
+
+
+
+
         }
+
+        
+
     }
 }
